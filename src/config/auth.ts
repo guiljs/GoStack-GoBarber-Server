99d73277f92ec13784ce8ed1777675152962f281
Guilherme de Jesus Santos <guiljs@gmail.com>
2020-06-27 10:52:51 -0300

    Creating file upload with multer

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '1907ba61d848420645072f8ed0f1e6e3',
+    expiresIn: '1d',
+  },
+};
---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import routes from './routes';
+import 'reflect-metadata';
+import './database';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+app.get('/', (request, response) =>
+  response.json({ message: 'Hello GoStack!' }),
+);
+
+app.listen(3333, () => {
+  console.log('🚀 Server started on port 3333!');
+  console.log('Teste');
+});
