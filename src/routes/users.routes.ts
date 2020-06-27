19fd3385136856697121bd38c42dc6cbb7bed4cd
Guilherme de Jesus Santos <guiljs@gmail.com>
2020-06-27 11:44:34 -0300

    File upload through API finished

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
+
 import CreateUserService from '../services/CreateUserService';
 import ensuredAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -27,7 +31,9 @@ usersRouter.post('/', async (request, response) => {
 usersRouter.patch(
   '/avatar',
   ensuredAuthenticated,
+  upload.single('avatar'),
   async (request, response) => {
+    console.log(request.file);
     return response.json({ ok: true });
   },
 );
