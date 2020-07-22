d3aa846fcc7513f43e3816a595069572d0fca8a6
Guilherme de Jesus Santos <guiljs@gmail.com>
2020-07-21 23:20:45 -0300

    First test with Jest using a fake repository

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,10 +1,21 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
 describe('CreateAppointment', () => {
-  it('should be able to create a new appointment', () => {
-    expect(1 + 2).toBe(3);
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentRepository = new FakeAppointmentsRepository();
+    const createAppointmentService = new CreateAppointmentService(
+      fakeAppointmentRepository,
+    );
+    const appointment = await createAppointmentService.execute({
+      date: new Date(),
+      provider_id: '5031',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('5031');
   });
-  it('should not be able to create two appointments on the same time', () => {
-    expect(1 + 2).toBe(3);
-  });
+  // it('should not be able to create two appointments on the same time', () => {
+  //   expect(1 + 2).toBe(3);
+  // });
 });
