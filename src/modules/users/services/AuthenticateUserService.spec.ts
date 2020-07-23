2ad39c045658b52adc2a34dae6d75b14e0b40d97
Guilherme de Jesus Santos <guiljs@gmail.com>
2020-07-22 22:59:46 -0300

    Testing user not created and wrong password

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -28,4 +28,43 @@ describe('CreateUser', () => {
     expect(response).toHaveProperty('token');
     expect(response.user).toEqual(user);
   });
+
+  it('should not be able to authenticate with non existing user', async () => {
+    const hashProvider = new FakeHashProvider();
+    const fakeUsersRepository = new FakeUsersRepository();
+    const authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      hashProvider,
+    );
+
+    expect(
+      authenticateUser.execute({
+        email: 'johndoe@example.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to authenticate with wrong password', async () => {
+    const hashProvider = new FakeHashProvider();
+    const fakeUsersRepository = new FakeUsersRepository();
+    const createUser = new CreateuserService(fakeUsersRepository, hashProvider);
+    const authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      hashProvider,
+    );
+
+    await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    expect(
+      authenticateUser.execute({
+        email: 'johndoe@example.com',
+        password: 'wrong-password',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
