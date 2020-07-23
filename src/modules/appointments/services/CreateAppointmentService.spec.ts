58344f5b41e4071af7cd8e63baa7e9aa1adcfd36
Guilherme de Jesus Santos <guiljs@gmail.com>
2020-07-22 22:22:26 -0300

    Users nad Appointments tests

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,5 @@
+import AppError from '@shared/errors/AppError';
+
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
@@ -15,7 +17,23 @@ describe('CreateAppointment', () => {
     expect(appointment).toHaveProperty('id');
     expect(appointment.provider_id).toBe('5031');
   });
-  // it('should not be able to create two appointments on the same time', () => {
-  //   expect(1 + 2).toBe(3);
-  // });
+
+  it('should not be able to create two appointments on the same time', async () => {
+    const fakeAppointmentRepository = new FakeAppointmentsRepository();
+    const createAppointmentService = new CreateAppointmentService(
+      fakeAppointmentRepository,
+    );
+    const appointmentDate = new Date(2020, 6, 21, 23);
+    await createAppointmentService.execute({
+      date: appointmentDate,
+      provider_id: '5031',
+    });
+
+    expect(
+      createAppointmentService.execute({
+        date: appointmentDate,
+        provider_id: '5031',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
