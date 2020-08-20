c9e90c004e2cbe8b5256605395d9547e6b5e7176
Guilherme de Jesus Santos <guiljs@gmail.com>
2020-08-19 23:16:05 -0300

    Transforming user entities with Class-transformer

--- src/modules/users/infra/http/controllers/ProfileController.ts
+++ src/modules/users/infra/http/controllers/ProfileController.ts
@@ -2,13 +2,13 @@ import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 import UpdateProfileService from '@modules/users/services/UpdateProfileService';
 import ShowProfileService from '@modules/users/services/ShowProfileService';
+import { classToClass } from 'class-transformer';
 
 export default class ProfileController {
   public async show(request: Request, response: Response): Promise<Response> {
     const showProfile = container.resolve(ShowProfileService);
     const profile = await showProfile.execute(request.user.id);
-    delete profile.password;
-    return response.json(profile);
+    return response.json(classToClass(profile));
   }
 
   public async update(request: Request, response: Response): Promise<Response> {
@@ -25,8 +25,6 @@ export default class ProfileController {
       old_password,
     });
 
-    delete updatedProfile.password;
-
-    return response.json(updatedProfile);
+    return response.json(classToClass(updatedProfile));
   }
 }
