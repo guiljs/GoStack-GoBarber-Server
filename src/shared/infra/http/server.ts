c1ecaa4204b9fc218771f234cdbcc497d9126500
Guilherme de Jesus Santos <guiljs@gmail.com>
2020-08-22 13:02:40 -0300

    Added RateLimit middleware to limit requests per second

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -10,9 +10,10 @@ import AppError from '@shared/errors/AppError';
 import '@shared/infra/typeorm';
 import '@shared/container';
 import routes from './routes';
+import rateLimiter from './middlewares/rateLimiter';
 
 const app = express();
-
+app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
@@ -36,5 +37,5 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
 });
 
 app.listen(3333, () => {
-  console.log('🚀 Server started on port 3333!');
+  console.log('🚀  Server started on port 3333!');
 });
